1ceb255b0cc80d085f83707b3f6d92e001529421
Nico Forero <ns.forero10@gmail.com>
2020-03-11 21:57:07 -0500

    [MVP] minimun viable product ready to go

--- src/App.js
+++ src/App.js
@@ -1,31 +1,111 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import 'antd/dist/antd.css';
 import './App.css';
-import {Layout, Menu, Table} from 'antd';
+import {Layout, Table, notification} from 'antd';
 
 function App() {
+
+  const [firstTime,setfirstTime]= useState(true);
+  const [dataTable,setDataTable] = useState([])
+  const columns = [
+    {
+      title: 'Date',
+      dataIndex: 'date',
+      key: 'date',
+    },
+    {
+      title: 'Home team',
+      dataIndex: 'home_team',
+      key: 'home_team'
+    },
+    {
+      title: 'Home Goals',
+      dataIndex: 'home_ft',
+      key: 'home_ft'
+    },
+    {
+      title: 'Away Goals',
+      dataIndex: 'away_ft',
+      key: 'away_ft'
+    },
+    {
+      title: 'Away team',
+      dataIndex: 'away_team',
+      key: 'away_team'
+    },
+    {
+      title: 'City',
+      dataIndex: 'city',
+      key: 'city'
+    },
+    {
+      title: 'Country',
+      dataIndex: 'country',
+      key: 'country'
+    }
+  ]
+  const getDataMatches = matches => {
+    return matches.map(match =>({
+      key: match.id,
+      date: match.date,
+      home_team: match.home_team,
+      home_ft: match.home_ft,
+      away_ft: match.away_ft,
+      away_team: match.away_team,
+      city: match.city,
+      country: match.country,
+      tournament: match.tournament
+    }))
+  }
+  const getMatches = async (params = {}) => {
+    var targeturl = 'https://matches-server.herokuapp.com/v1/matchs',
+        proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+    
+    await fetch (proxyUrl + targeturl, {
+      header:{
+        accept: "application/json"
+        },
+      }).then(response =>{
+      if(!response.ok){
+        throw new Error(response.statusText)
+      }
+      return response
+    }).then(async response =>{
+      const data = await response.json()
+
+
+      setDataTable(getDataMatches(data.matchs))
+    }).catch(error => {
+      notification.error({
+        message: 'Opps!',
+        description: 'There is a problem parsing the data'
+      })
+
+    })
+  }
+  useEffect(() => {
+    if (firstTime){
+    getMatches()
+    setfirstTime(false)
+  }
+    const interval = setInterval(()=>{
+      getMatches()
+    }, 20000)
+    return () => clearInterval(interval)
+  },[])
+
   return (
     
     <div className='main'>
     <Layout class = 'layout'>
       <Layout.Header>
       <h1 style={{color:"white"}}>Partidos</h1>
-        <Menu
-        theme="dark"
-        mode = "horizontal"
-        defaultSelectedKey = {[2]}
-        style = {{lineHeight: '64px'}}
-        >
-          <Menu.Item key="1">nav 1</Menu.Item>
-          <Menu.Item key="2">nav 2</Menu.Item>
-          <Menu.Item key="3">nav 3</Menu.Item>
-        </Menu>
       </Layout.Header>
       <Layout.Content>
-        <Table>
-          
-        </Table>
-        <div className="site-layout-content">Content</div>
+        <Table
+          columns={columns}
+          dataSource={dataTable}
+        />
       </Layout.Content>
       <Layout.Footer>
         <span> Made with love by Nicolas Forero</span>
